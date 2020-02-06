563e095e05ba1186b096b05e6efc22eb7dd2abfc
anewhopeee <60725228+anewhopeee@users.noreply.github.com>
2020-02-05 23:06:04 -0500

    Create Project.cs

---
+++ Project.cs
@@ -0,0 +1,40 @@
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGBot
+{
+    class Program
+    {
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+        public async Task Start()
+        {
+            Client = new DiscordSocketClient();
+
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, "Njc0Nzk1MDA3MTQzNTc1NTcy.XjtziA.NbtrzTox5n9MeYvZDjpLV-JMsfI", true);
+
+            await Client.StartAsync();
+
+            await Handler.Install(Client);
+
+            Client.Ready += Client_Ready;
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Ready()
+        {
+            Console.WriteLine("The bot is online.");
+            return;
+        }
+
+    }
+}
+
